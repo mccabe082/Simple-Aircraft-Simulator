bb14592d385a33ea9df461af28ba0b4b49c33bea
david.mccabe <david.mccabe082@gmail.com>
2020-05-09 17:16:06 +0100

    addde testing for linear interpolation

---
+++ src/interpolate_test/linear_lookup.test.cpp
@@ -0,0 +1,29 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <interpolate/linear_lookup.h>
+
+using namespace interp;
+
+TEST_CASE("Linear lookup works using linear interpolation", "[linear_lookup]")
+{
+
+	SECTION("Querying a point precisely returns the exact value in the table")
+	{
+		REQUIRE(false);
+	}
+
+	SECTION("Check that the interpolation is linear")
+	{
+		REQUIRE(false);
+	}
+
+	SECTION("Check that we can extrapolate smaller values")
+	{
+		REQUIRE(false);
+	}
+
+	SECTION("Check that we can extrapolate larger values") {
+
+	}
+
+}

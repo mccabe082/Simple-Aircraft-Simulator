68f9946a58e968a72d85250b4e448688f44648cf
david.mccabe <david.mccabe082@gmail.com>
2020-05-16 10:41:07 +0100

    added better bilinear test case

--- src/interpolate_test/bilinear_lookup.test.cpp
+++ src/interpolate_test/bilinear_lookup.test.cpp
@@ -7,20 +7,27 @@ TEST_CASE("Simple bilinear lookup tests", "[bilinear_lookup]")
 {
 	auto testTable = BilinearLookup::load("D:/simple-aircraft-simulator/src/interpolate_test/test2d.xml");
 
-	auto testFunc = [](double x, double y) {return exp(-pow(x, 2) + pow(y, 2)); };
+	auto testFunc = [](double x, double y) {return x * (y + .3); };
 
 	SECTION("Test exact lookups are recovered perfectly")
 	{
 		double x = -0.8;
 		double y = -0.4;
-		double expected = 0.618783392;
-		REQUIRE(testTable(x,y)==Approx(expected));
+		double expected = 0.08;
+		REQUIRE(testTable(x, y) == Approx(expected));
 	}
 
-	SECTION("Test lookups are accurate")
+	SECTION("interpolation works correctly")
 	{
-		double x = 1.-sqrt(2.);
+		double x = 1. - sqrt(2.);
 		double y = exp(-3.);
-		REQUIRE(testTable(x, y) == Approx(testFunc(x,y)));
+		REQUIRE(testTable(x, y) == Approx(testFunc(x, y)));
+	}
+
+	SECTION("Extrapolation works correctly")
+	{
+		double x = 123.45;
+		double y = -543.21;
+		REQUIRE(testTable(x, y) == Approx(testFunc(x, y)));
 	}
 }

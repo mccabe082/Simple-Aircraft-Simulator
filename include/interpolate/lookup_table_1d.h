4eef6d917f43b30eed4ea4156be1b6a9f960f96f
david.mccabe <david.mccabe082@gmail.com>
2020-05-17 13:36:46 +0100

    removing unused function

--- include/interpolate/lookup_table_1d.h
+++ include/interpolate/lookup_table_1d.h
@@ -9,14 +9,6 @@ namespace interp
 	{
 	public:
 		virtual double operator()(double x) const = 0;
-		bool addDataPoint(double x, double f)
-		{
-			if (data.empty() || x > data.back().x) {
-				data.push_back(DataPoint{ x, f });
-				return true;
-			}
-			return false;
-		}
 
 		struct DataPoint
 		{
@@ -25,10 +17,10 @@ namespace interp
 		};
 		using DataTable = std::vector<DataPoint>;
 
-		size_t size() const { return data.size(); }
-		void resize(size_t i, DataPoint p) { data.resize(i, p); }
 		DataPoint operator[](size_t i) const { return data[i]; }
 		DataPoint& operator[](size_t i) { return data[i]; }
+		size_t size() const { return data.size(); }
+		void resize(size_t i, DataPoint p) { data.resize(i, p); }
 		void clear() { data.clear(); }
 
 	protected:
